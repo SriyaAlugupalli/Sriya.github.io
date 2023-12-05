4fe8c7fe15c6044f8b0571c1f6d16958f5753b01
SriyaAlugupalli <145814664+SriyaAlugupalli@users.noreply.github.com>
2023-12-04 19:48:40 -0500

    Add files via upload

---
+++ ChatGPTProject1/test.js
@@ -0,0 +1,10 @@
+function showVideo() {
+    var video = document.getElementById('restaurantVideo');
+    video.style.display = 'block';
+    video.play();
+}
+
+function submitForm() {
+    // Add form submission logic here
+    alert('Form submitted!');
+}
